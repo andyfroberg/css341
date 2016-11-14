10e3158e667c88133efde1527b8ab406e789084b
andyfroberg <andy.froberg@gmail.com>
2016-11-14 15:07:58 -0800

    Update hw5.py

--- hw5.py
+++ hw5.py
@@ -3,7 +3,7 @@ import numpy
 
 # 1
 # Calculates and prints out the correlation between each combination of the 
-# three indices, i.e., between nasdaq and sp500, nasdaq and djia, and sp500 and 
+# three indices, e.g., between nasdaq and sp500, nasdaq and djia, and sp500 and 
 # djia. This calculation cannot make use of any pre-written calculation 
 # functions from SciPy, NumPy, matplotlib, pandas, etc. (You can, however, use 
 # functions like shape, ravel, etc.) That is to say, you have to write your own 
@@ -11,22 +11,75 @@ import numpy
 # them) in the .py file.  (If you don't import scipy, etc., you'll be fine.)  
 # Note that if you do this task by writing a correlation calculating function, 
 # it will help you do #2 below.
-def correlation(list1, list2):
+def correlation(x, y):
     """Calculates and prints the correlation between stock index lists.
     
     This function calculates and prints the correlation between two input lists
-    (of stock indices). 
+    (of stock indices). Calculation for lag correlation is as follows (square
+    brackets are for mathematical clarity and do not denote array syntax or
+    any other programming data structures):
+        
+        r_xy = [cov(x,y)] / [stddev(x) * stdev(y)]
+             = [sum i=1 to n ((x_i - x_bar) (y_i - y_bar))]
+               / [sqrt(sum i=1 to n (x_i - x_bar)^2 
+               * sum i=1 to n (y_i - y_bar)^2)] 
     
     Postitional Input Parameters:
-        list1 | list (float):
+        x | list (float):
             A list of stock index prices. Assumed to be floats.
-        list2 | list (float):
+        y | list (float):
             A list of stock index prices. Assumed to be floats.
         
     Returns:
         correlation | float:
             The correlation coefficient of two input stock indices.
     """
+    #x_array = numpy.array(x, dtype='f') # make sure input is array
+    #y_array = numpy.array(y, dtype='f')
+    x_tot = 0.0
+    y_tot = 0.0
+    for i in range(len(x)):
+        x_tot += x[i]
+        y_tot += y[i]
+    x_bar = x_tot / len(x)
+    y_bar = y_tot / len(y)
+    covariance = 0.0
+    for i in range(len(x)):
+        covariance += (x[i] - x_bar) * (y[i] - y_bar)
+    x_var = 0.0
+    y_var = 0.0
+    for i in range(len(x)):
+        x_var += (x[i] - x_bar)**2
+        y_var += (y[i] - y_bar)**2
+    x_sig = x_var**(0.5)
+    y_sig = y_var**(0.5)
+    return covariance / (x_sig * y_sig)
+
+# test with numpy correlate function
+print(numpy.correlate(stocks.nasdaq, stocks.sp500))
+
+# Print the three correlation between combinations of indices
+# Correlation between Nasdaq and S&P500
+print(correlation(stocks.nasdaq, stocks.sp500))
+# Correlation between Nasdaq and DJIA
+print(correlation(stocks.nasdaq, stocks.djia))
+# Correlation between S&P500 and DJIA
+print(correlation(stocks.sp500, stocks.djia))    
+
+# 2
+# Calculates the lag autocorrelation for each of the three indices. You can 
+# consider only positive lags if you're correlating f(t) vs. f(t+lag), where f 
+# is the NASDAQ, S&P500, or DJIA index.  Again, you cannot use calculation 
+# functions you have not written yourself. (You can, however, use functions 
+# like shape, ravel, etc.)
+def autocorrelation(x, lag):
+    """Calculates the lag autocorrelation of a given stock index (given some lag).
+    
+    This function calculates the lag autocorrelation 
+    
+    """    
+    
+    
     
     
     
